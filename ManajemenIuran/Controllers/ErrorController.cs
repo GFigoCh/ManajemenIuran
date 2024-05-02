54fa3101b84c29300174afb4af80f480c9f33cab
GFigoCh <gfigoch@outlook.com>
2024-05-02 15:04:16 +0700

    Pembersihan template project.

---
+++ ManajemenIuran/Controllers/ErrorController.cs
@@ -0,0 +1,15 @@
+﻿using ManajemenIuran.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ManajemenIuran.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Index()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
